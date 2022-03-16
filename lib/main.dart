be41c805841221d0676f2320bc16e1bd409adf8b
Naush <abuzaid.naushad@gmail.com>
2022-03-16 19:22:22 +0530

    day 4 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: import_of_legacy_library_into_null_safe
+
 import "package:flutter/material.dart";
 import 'package:flutter_catalog/pages/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(myApp());
@@ -13,7 +16,10 @@ class myApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
+      theme: ThemeData(
+        primarySwatch: Colors.deepPurple,
+        fontFamily: GoogleFonts.lato().fontFamily,
+      ),
       darkTheme: ThemeData(brightness: Brightness.dark),
       initialRoute: "/home",
       routes: {
